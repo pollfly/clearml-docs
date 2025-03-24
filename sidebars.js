83de2882e37939f7ca2986cc6b5388853c30ae1b
revital <revital@allegro.ai>
2025-03-24 15:05:35 +0200

    Add allegroai 3.13 release notes

--- sidebars.js
+++ sidebars.js
@@ -351,9 +351,10 @@ module.exports = {
             {
                 'Enterprise':
                         [
-                           'release_notes/sdk/enterprise/ver_3_12',
+                           'release_notes/sdk/enterprise/ver_3_13',
                            {
                                 'Older Versions': [
+                                   'release_notes/sdk/enterprise/ver_3_12',
                                    'release_notes/sdk/enterprise/ver_3_11',
                                    'release_notes/sdk/enterprise/ver_3_10',
                                 ]
