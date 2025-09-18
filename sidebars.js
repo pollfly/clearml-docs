c4a36ad276c7efc2e843f7d5af81dea79c557744
revital <revital@allegro.ai>
2025-09-18 15:34:54 +0300

    Add Enterprise Server 3.27.0 release notes

--- sidebars.js
+++ sidebars.js
@@ -315,9 +315,10 @@ module.exports = {
             {
                 'Enterprise':
                         [
-                           'release_notes/clearml_server/enterprise/ver_3_26',
+                           'release_notes/clearml_server/enterprise/ver_3_27',
                            {
                                 'Older Versions': [
+                                     'release_notes/clearml_server/enterprise/ver_3_26',
                                      'release_notes/clearml_server/enterprise/ver_3_25', 'release_notes/clearml_server/enterprise/ver_3_24',
                                      'release_notes/clearml_server/enterprise/ver_3_23', 'release_notes/clearml_server/enterprise/ver_3_22',
                                      'release_notes/clearml_server/enterprise/ver_3_21', 'release_notes/clearml_server/enterprise/ver_3_20'
